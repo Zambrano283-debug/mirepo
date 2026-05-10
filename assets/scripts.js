dd2da8155f89b2d76bfcbfe77bfcd8413adc7148
davidromanl <roman.david@gmail.com>
2026-05-10 12:06:39 -0500

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,8 @@
 function validarEdad() {
-    alert("Eres mayor o menor de edad")
+    var edad = document.getElementById("edad")
+    if (edad.value > 18 ) {
+        alert("Eres mayor de edad")
+    } else {
+        alert("Eres menor de edad")
+    }
 }
